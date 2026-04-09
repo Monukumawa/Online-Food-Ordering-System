4fb951d6e1a7bf41974f3d957cc5cb992c210a1e
Monukumawa <monukumwat14@gmail.com>
2026-04-10 01:04:19 +0530

    Update API URL and CORS settings for deployment

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer} from 'react-toastify';
 // import 'react-toastify/dist/React/ReactToastify.css'
 
 const App = () => {
-  const url ="http://localhost:4000";
+ const url = "https://online-food-ordering-system-1km0.onrender.com"
   return (
     <div>
     <ToastContainer/>
